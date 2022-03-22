5b2bceb4bce3a37154f633c88e0a20fcf9f52cad
veenareddye61 <101815704+veenareddye61@users.noreply.github.com>
2022-03-22 19:36:20 +0530

    Update checkout.js

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,29 +1,69 @@
 var mainCartDiv= $("#main-cart-div")
 var cartDiv1 = $("#cart-div1")
-var cartDiv2 = document.getElementById("cart-div2")
+/*var cartDiv2 = document.getElementById("cart-div2")*/
 
+var prodCard
 prodCard=JSON.parse(localStorage.getItem("prodCard"));
-console.log(prodCard);
+console.log(prodCard)
+$('#itemCount').text(prodCard.length)
+$("#cart-count").text(prodCard.length);
+
+var sum=0
+
+/*cartDivProd2 = document.createElement("div")*/
 
 
-for(i=0;i<=prodCard.length;i++){
-    var cartDivProd
-    $('#itemCount').text(prodCard[i].numberOf+prodCard[i].numberOf)  
-cartDivProd =`<div class="chcard"><div class="chcard1"><img class="prodImg" src="${prodCard[i].src}" ></img>
+var prodCard2 = [];
+var number=0
+for(i=0;i<prodCard.length;i++){
+    sum+=parseInt(prodCard[i].price);
+    $("#totamt2").text(sum);
+    var number=parseInt(prodCard[i].numberOf)
+    console.log(number)
+    if(number>1)
+    {
+        prodCard2.pop();
+    }
+   prodCard2.push(prodCard[i])
+}
+console.log(prodCard2)
+for(j=0;j<prodCard2.length;j++){
+     cartDivProd =`<div class="chcard"><div class="chcard1"><img class="prodImg" src="${prodCard2[j].src}" ></img>
+</div>
+<div class="chcard2"><h4 class="prodName">${prodCard2[j].name}</h4>
+<p class= "prodNum">${'x'+prodCard2[j].numberOf}</p>
+<p class="prodPrice">${'Amount Rs:'+(prodCard2[j].numberOf*prodCard2[j].price)}</p></div></div>`
+cartDiv1.append(cartDivProd)
+}
+
+
+
+
+/*cartDivProd =`<div class="chcard"><div class="chcard1"><img class="prodImg" src="${prodCard[i].src}" ></img>
 </div>
 <div class="chcard2"><h4 class="prodName">${prodCard[i].name}</h4>
 <p class= "prodNum">${'x'+prodCard[i].numberOf}</p>
-<p class="prodPrice">${'Amount Rs:'+(prodCard[i].numberOf*prodCard[i].price)}</p></div></div>`
-cartDiv1.append(cartDivProd)
-cartDiv2.innerHTML=`<div class="totalAmtCard">
-                <p>Total Amount</p>
-                <p>Amount:</P>
-                <a href="../confirm/confirm.html"><button id="placeOrder" onClick="orderPlaced">Place Order</button></div></a>`
+<p class="prodPrice">${'Amount Rs:'+(prodCard[i].numberOf*prodCard[i].price)}</p></div></div>`}
+else{
+    cartDivProd =`<div class="chcard"><div class="chcard1"><img class="prodImg" src="${prodCard[i-1].src}" ></img>
+</div>
+<div class="chcard2"><h4 class="prodName">${prodCard[i-1].name}</h4>
+<p class= "prodNum">${'x'+prodCard[i-1].numberOf}</p>
+<p class="prodPrice">${'Amount Rs:'+(prodCard[i-1].numberOf*prodCard[i-1].price)}</p></div></div>`
 }
+cartDiv1.append(cartDivProd)
+sum+=parseInt(prodCard[i-1].price);
+$("#totamt2").text(sum);
+}
+console.log(cartDivProd.length)*/
+function orderPlaced(){
+    
+    $("#cart-count").text("0");
+    localStorage.clear();
+    
+}
+ 
 
-/*function orderPlaced(){
-    location.assign()
-}*/
 
 
 
